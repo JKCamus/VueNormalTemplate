df9e3fbbcc73bbb996f198e3dc9c2421bd224981
JKCamus <924850758@qq.com>
2020-10-31 09:38:59 +0800

    picture update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/home">首页</router-link>
       <router-link to="/login">登录</router-link>
     </div>
-    <img alt="Vue logo" src="./assets/img/logo.png" />
+    <img alt="freedom" src="./assets/img/freedom.png" style="width: 500px" />
     <HelloWorld msg="camus's vue template" />
   </div>
 </template>
